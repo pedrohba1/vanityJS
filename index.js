9b5ea06f1256c33cefd6cf492b97627187efee1b
pedrohba1 <pedro.almeida@ufu.br>
2021-07-28 23:02:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var crypto = require('crypto');
+const bs58 = require('bs58')
+var md5sum = crypto.createHash('md5');
+
+async function generateB58() {
+    const seed =  'secret' + Math.random().toString(); // muda o seed pra ser com date também
+    const sha = crypto.createHash('sha256');  
+    hash = sha.update(seed).digest('hex')
+    const bytes = Buffer.from(hash, 'hex')
+    const base58hash = bs58.encode(bytes)
+    return {seed, base58hash}
+}
+
+let index = 0;
+async function findVanity(stringArr){
+    while(true){
+        let {base58hash, seed} = await generateB58();
+        stringArr.forEach(string =>{
+            if(base58hash.includes(string)){
+                index += 1;
+                console.log(`#${index}`);
+                console.log(`seed: ${seed}`);
+                console.log(`string encontrada: ${string}`)
+                console.log(`hash em base58: ${base58hash}`);     
+            }
+        })
+    }    
+}
+
+const names = ['pedro', 'p3dro', 'p3dr0', 'pedr0', 'p3dr0', 'p3dru', 'pedru'];
+findVanity(names);
+
+
+
+
+
